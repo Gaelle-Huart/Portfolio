0c83d282870aa763f4034b0c6bbd8b00863265bc
Gaelle Huart <gaelle.c.huart@gmail.com>
2026-01-25 15:58:36 +0100

    Section Skills mise en page (ajouts possibles)
    Section Projects à remanier
    
    Hero et About combinés (moins lourd)

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -3,42 +3,52 @@ import styles from './styles.module.css';
 import Card from '../Card/index';
 
 
+const SkillList = [
+  {
+    Svg:require('@site/static/img/HTML5_logo.svg').default
+  },
+  {
+    Svg:require('@site/static/img/CSS3_logo.svg').default
+  },
+  {
+    Svg:require('@site/static/img/JavaScript_logo.svg').default
+  },
+  {
+    Svg:require('@site/static/img/React-icon.svg').default
+  },
+  {
+    Svg:require('@site/static/img/redux-logo.svg').default
+  },
+  {
+    Svg:require('@site/static/img/Git_icon.svg').default
+  },
+  {
+    Svg:require('@site/static/img/github-noir.svg').default
+  },
+  {
+    Svg:require('@site/static/img/Sass_Logo_Color.svg').default
+  },
+]
+
+function Skill({Svg}) {
+  return (
+    <div className='skill'>
+      <Card shadow='tl' className={styles.skillContainer}>
+        <Svg className={styles.skillSvg} role="img" />
+      </Card>
+    </div>
+  );
+}
+
 export default function Skills() {
   return (
     <section className={styles.section}>
       <div className="container">
         <Heading id="projects" as="h2" className={styles.title}>Skills</Heading>
-        <div className="inline">
-          <Card shadow='tl' className={styles.cardContainer}>
-            <img
-              src={require('@site/static/img/HTML5.png').default}
-              alt="HTML5"
-            />
-          </Card>
-          <Card shadow='tl' className={styles.cardContainer}>
-              <img
-                src={require('@site/static/img/CSS3.png').default}
-                alt="CSS3"
-              />
-          </Card>
-          <Card shadow='tl' className={styles.cardContainer}>
-            <img
-              src={require('@site/static/img/JavaScript-logo.png').default}
-              alt="JavaScript"
-            />
-          </Card>
-          <Card shadow='tl' className={styles.cardContainer}>
-            <img
-              src={require('@site/static/img/React-icon.svg').default}
-              alt="React"
-            />
-          </Card>
-          <Card shadow='tl' className={styles.cardContainer}>
-            <img
-              src={require('@site/static/img/redux-logo.png').default}
-              alt="Redux"
-            />
-          </Card>
+        <div className="inlineSkills">
+          {SkillList.map((props, idx) => (
+            <Skill key={idx} {...props} />
+          ))}
         </div>
       </div>
     </section>
