b969d783ed9d3163db0f1047dd3b0842b7119171
Gaelle Huart <gaelle.c.huart@gmail.com>
2026-02-19 18:35:57 +0100

    Corrections, focus et CSS
    - suppression du fichier record.md (inutile)
    - ajout de onBrokenMarkdownLinks: 'warn' dans docusaurus.config.js pour éviter que les liens cassés dans les fichiers markdown ne bloquent le build
    - ajout de onBrokenAnchors: 'ignore' dans docusaurus.config.js pour éviter que les liens d'ancrage cassés ne bloquent le build
    - focus remanié sur les fields et textarea du formulaire de contact pour les rendre plus visibles par défaut
    - affinage du focus sur les projets pour les rendre plus visibles et assurer le comportement attendu
    - la classe .active est appliquée aussi sur la version sombre du thème pour les boutons du Hero

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -31,6 +31,8 @@ const config = {
   projectName: 'Portfolio', // Usually your repo name.
 
   onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  onBrokenAnchors: 'ignore',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
